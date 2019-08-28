d4ed84208760f74d98eef1ec31be9a67d13ee3ef
kyung221 <roodwl@naver.com>
2019-08-28 10:54:25 +0900

    2164 카드2

---
+++ study/study/2164.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n;
+	queue<int> q;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	while (q.size() != 1) {
+		q.pop();
+		int k = q.front();
+		q.pop();
+		q.push(k);
+	}
+	cout << q.front()<<'\n';
+}
