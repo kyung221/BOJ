be002300d5c26059e3f6017b7b9c8446634de8b7
kyung221 <roodwl@naver.com>
2019-08-28 11:46:36 +0900

    11866 조세퍼스 문제 0

---
+++ study/study/11866.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int main() {
+	int n, k;
+	queue < int> q;
+	vector<int> v;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	//원형 큐를 만들어 해결
+	while (!q.empty()) {
+		for (int i = 0; i < k - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		v.push_back(q.front());
+		q.pop();
+	}
+
+	cout << "<";
+	for (int i = 0; i < n; i++) {
+		cout << v[i];
+		if (i != n - 1)cout << ", ";
+	}
+	cout << ">\n";
+}
+
