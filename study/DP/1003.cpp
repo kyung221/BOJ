b257c93badcb12a5f9acda7dbc17068413b9e971
kyung221 <roodwl@naver.com>
2019-09-03 18:12:46 +0900

    1003 피보나치 함수

---
+++ study/DP/1003.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	int c0[41] = { 1,0 }; //0의 개수
+	int c1[41] = { 0,1 }; //1의 개수
+	
+	for (int i = 2; i <= 40; i++) {
+		c0[i] = c0[i - 2] + c0[i - 1];
+		c1[i] = c1[i - 2] + c1[i - 1];
+	}
+	for (int i = 0; i < t; i++) {
+		int n;
+		cin >> n;
+		cout << c0[n] << " " << c1[n] << '\n';
+	}
+
+}
